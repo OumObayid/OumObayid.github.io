2f192985a84290b3ddc1818d72ab438d66181e54
Oumaima El Obayid <elobayidoumaima@gmail.com>
2021-07-02 12:28:07 +0100

    suite

--- public/js/main.js
+++ public/js/main.js
@@ -41,25 +41,22 @@ $(document).ready(function () {
         return false;
     });
 
-    
-
-// to change active on scroll
  // Navigation active state on scroll
- var nav_sections = $('section');
- var main_nav = $('.navbar');
+ var sections = $('section');
+ var navbar = $('.navbar');
 
  $(window).on('scroll', function() {
    var cur_pos = $(this).scrollTop() + 200;
 
-   nav_sections.each(function() {
+   sections.each(function() {
      var top = $(this).offset().top,
        bottom = top + $(this).outerHeight();
 
      if (cur_pos >= top && cur_pos <= bottom) {
        if (cur_pos <= bottom) {
-         main_nav.find('li').removeClass('active');
+        navbar.find('li').removeClass('active');
        }
-       main_nav.find('a[href="#' + $(this).attr('id') + '"]').parent('li').addClass('active');
+       navbar.find('a[href="#' + $(this).attr('id') + '"]').parent('li').addClass('active');
      }
      if (cur_pos < 300) {
        $(".navbar ul:first li:first").addClass('active');
