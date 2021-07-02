6eced88a9cf33bdb271e1290ad6b46428e21921d
Oumaima El Obayid <elobayidoumaima@gmail.com>
2021-07-02 18:49:27 +0100

    suite

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,21 @@
 //initialisation aos
 AOS.init(); 
-
+//fonction onscroll() for Navigation active state on scroll
+function onScroll(){
+  var scrollPosition = $(document).scrollTop();
+  $('.navbar-nav a').each(function () {
+    var currentLink = $(this);
+    var refElement = $(currentLink.attr("href"));
+    if (refElement.position().top <= scrollPosition && refElement.position().top + refElement.height() > scrollPosition) {
+      $('.navbar-nav li a').removeClass("active");
+      currentLink.addClass("active");
+    }
+    else{
+      currentLink.removeClass("active");
+    }
+  });
+}
+//jquery
 $(document).ready(function () {  
     //mettre la page au top lors de son rechargement
     $('html, body').animate({ 
@@ -42,27 +57,28 @@ $(document).ready(function () {
     });
 
  // Navigation active state on scroll
- var sections = $('section');
- var navbar = $('.navbar');
+        $(document).on("scroll", onScroll);
+				$('a[href^="#"]').on('click', function (e) {
+					e.preventDefault();
+					$(document).off("scroll");
 
- $(window).on('scroll', function() {
-   var cur_pos = $(this).scrollTop() + 100;
+					$('a').each(function () {
+						$(this).removeClass('active');
+					})
+					$(this).addClass('active');
 
-   sections.each(function() {
-     var top = $(this).offset().top,
-       bottom = top + $(this).outerHeight();
-
-     if (cur_pos >= top && cur_pos <= bottom) {
-       if (cur_pos <= bottom) {
-        navbar.find('li').removeClass('active');
-       }
-       navbar.find('a[href="#' + $(this).attr('id') + '"]').parent('li').addClass('active');
-     }
-     if (cur_pos < 300) {
-       $(".navbar ul:first li:first").addClass('active');
-     }
-   });
- });
+					var target = this.hash;
+					$target = $(target);
+					$('html, body').stop().animate({
+						'scrollTop': $target.offset().top
+					}, 500, 'swing', function () {
+						window.location.hash = target;
+						$(document).on("scroll", onScroll);
+					});
+				});
+        
+       
+ 
 
 
 
