9f32d6d317a91d372bba3269e26a278df14b287a
Oumaima El Obayid <elobayidoumaima@gmail.com>
2021-07-06 10:41:22 +0100

    suite

---
+++ public/js/config.js
@@ -0,0 +1,12 @@
+
+
+
+
+let btn = document.querySelector('.btn');
+let bodyBgProp = '--greenTheme';
+function changecolor() {
+        
+    	document.documentElement.style.setProperty(bodyBgProp,true);
+
+  }
+
