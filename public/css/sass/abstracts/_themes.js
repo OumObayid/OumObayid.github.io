eac849fd8ab3f11d2e7071bdfa83346456f6327e
Oumaima El Obayid <elobayidoumaima@gmail.com>
2021-07-08 09:49:01 +0100

    suite

---
+++ public/css/sass/abstracts/_themes.js
@@ -0,0 +1,12 @@
+
+    /*
+    |-------------------------------|
+    |-------theme parametres--------|
+    |-------------------------------|
+    */
+    // pour changer le theme mettre true à celui choisi et false aux reste
+    
+    
+    $blueTheme : true ;
+    $orangeTheme :false  ; 
+    $greenTheme :false  ;
