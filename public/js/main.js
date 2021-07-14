2abb9371bc63defe39ccd5b91737d9f90a9f458a
Oumaima El Obayid <elobayidoumaima@gmail.com>
2021-07-14 16:52:28 +0100

    suite

--- public/js/main.js
+++ public/js/main.js
@@ -1,34 +1,46 @@
- // Activate SimpleLightbox plugin for portfolio items
- new SimpleLightbox({
-  elements: '#portfolio a.portfolio-box'
+// Activate SimpleLightbox plugin for portfolio items
+new SimpleLightbox({
+    elements: '#portfolio a.portfolio-box'
 });
 
 
 
-//jquery code
-$(document).ready(function () {    
-    
-    // Init AOS
-  function aos_init() {
-    AOS.init({
-      duration: 800,
-      easing: "ease-in-out",
-      once: true
+/*============================================================================================*/
+/*=======================================jquery code==========================================*/
+/*============================================================================================*/
+$(document).ready(function() {
+
+    /*=======================================Initiation AOS======================================*/
+    function aos_init() {
+        AOS.init({
+            duration: 800,
+            easing: "ease-in-out",
+            once: true
+        });
+    }
+    $(window).on('load', function() {
+        aos_init();
     });
-  }
-  $(window).on('load', function() {
-    aos_init();
-  });
 
-     //navbar
-    $(document).scroll(function () {
-      var $nav = $(".navbar");
-      $nav.toggleClass('invisible', $(this).scrollTop() > 40 && $(this).scrollTop()<499);
-      $nav.toggleClass('scrolled', $(this).scrollTop() > 500);
-    }); 
-
-    // Un défilement animé (« Smooth scroll ») en jQuery sans plugin
-    $('a[href^="#"]').click(function () {
+    /*=====Setting for section Testimonials: Initiation of carousel (wl Carousel library)========*/
+    $(".testimonials-carousel").owlCarousel({
+        autoplay: true,
+        dots: true,
+        loop: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            768: {
+                items: 2
+            },
+            900: {
+                items: 3
+            }
+        }
+    });
+    /*===============================Setting smoothly scrolling===================================*/
+    $('a[href^="#"]').click(function() {
         var the_id = $(this).attr("href");
         if (the_id === '#') {
             return;
@@ -39,154 +51,118 @@ $(document).ready(function () {
         return false;
     });
 
-    // Back to top button
-    $(window).scroll(function() {
-        if ($(this).scrollTop() > 100) {
-        $('.back-to-top').fadeIn('slow');
+    /*============Navbar: ============Setting navbar on Mobile==================================*/
+    //-------------------invisibility of and visibility with background of navbar on scrolling bar
+    $(document).scroll(function() {
+        var $nav = $(".navbar");
+        $nav.toggleClass('invisible', $(this).scrollTop() > 40 && $(this).scrollTop() < 499);
+        $nav.toggleClass('scrolled', $(this).scrollTop() > 500);
+    });
+
+    //-----------add or remove background to navbar on clicking on button when scrollbar is on top
+    $('.navbar-toggler').click(function() {
+        if ($('.navbar-toggler').hasClass("collapsed")) {
+            $('.navbar').fadeIn('slow', function() {
+                $('.navbar').addClass('bgcolorMobilToggle');
+            });
         } else {
-        $('.back-to-top').fadeOut('slow');
+            $('.navbar').fadeOut('0', function() {
+                $(this).show();
+                $(this).removeClass('bgcolorMobilToggle');
+            });
         }
     });
+    //--------------navigation click on button mobile or on navbar link for hidding navbar collaps 
+    $('.nav-link').click(function() {
+        $('.navbar-toggler').click();
+    });
+
+    /*========Navbar:=======Navigation active state on scroll or on click on Desktop===========*/
+    //--------------------------------------------------------------------change navbar on scroll
+    var sections = $('.section'),
+        nav = $('.navbar'),
+        nav_height = nav.outerHeight(),
+        cur_pos = $(this).scrollTop();
+
+    $(window).on('scroll', function() {
+        sections.each(function() {
+            var top = ($(this).offset().top) - nav_height,
+                bottom = top + $(this).outerHeight();
+            //
+            if (cur_pos >= top && cur_pos <= bottom) {
+                nav.find('a').removeClass('active');
+                $(this).addClass('active');
+                nav.find('a[href="#' + $(this).attr('id') + '"]').addClass('active');
+            }
+        });
+        //when we are at the header
+        if (cur_pos >= 0 && cur_pos <= window.innerHeight - nav_height) {
+            nav.find('a').removeClass('active');
+            nav.find('a[href="#header"]').addClass('active');
+        }
+    });
+
+    //----------------------------------------------------------------change navbar on scrolling
+    nav.find('a').on('click', function() {
+        var $el = $(this),
+            id = $el.attr('href'),
+            deletHeight = 0;
+        if (cur_pos <= window.innerHeight && $el.attr('href') == "#apropos") deletHeight = 80; //80 to change for adaptation
+        $('html, body').animate({
+            scrollTop: ($(id).offset().top) - nav_height - deletHeight
+        }, 500);
+        return false;
+    });
+
+
+    /*=================================Setting back to top button=================================*/
+    //--------------------------------------fadeIn  and fadout button back to top on scrolling bar
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 100) {
+            $('.back-to-top').fadeIn('slow');
+        } else {
+            $('.back-to-top').fadeOut('slow');
+        }
+    });
+    //-------------------------------scrolling page to top on clicking on the button back to top
     $('.back-to-top').click(function() {
         $('html, body').animate({
-        scrollTop: 0
+            scrollTop: 0
         }, 1500);
         return false;
     });
 
- 
-  // Navigation active state on scroll or on click 
-  //change navbar on scroll
-    var sections = $('.section')
-    , nav = $('.navbar')
-    , nav_height = nav.outerHeight()
-    , cur_pos = $(this).scrollTop()
-    ;
-
-    $(window).on('scroll', function () {    
-        
-          sections.each(function() {
-            var top = ($(this).offset().top) - nav_height ,
-                bottom = top + $(this).outerHeight()  ;
-            
-            if (cur_pos >= top && cur_pos <= bottom) {
-              nav.find('a').removeClass('active');     
-              
-              $(this).addClass('active');
-              nav.find('a[href="#'+$(this).attr('id')+'"]').addClass('active');
-            }
-          });
-          if (cur_pos >= 0 && cur_pos <= window.innerHeight - nav_height) {
-            nav.find('a').removeClass('active');
-            nav.find('a[href="#header"]').addClass('active');
-          }
-        });
-
-//change navbar on click
-    nav.find('a').on('click', function () {
-        var $el = $(this)
-        , id = $el.attr('href')
-        ,deletHeight=0;
-        if(cur_pos <= window.innerHeight && $el.attr('href')=="#apropos" ) deletHeight=80;//80 to change for adaptation
-        $('html, body').animate({
-        scrollTop: ($(id).offset().top) - nav_height - deletHeight
-        }, 500); 
-        return false;
-    });
-    
-    //Skills progress bar animated 
+    /*==================Setting for section Skills: progress bar animated=====================*/
     var offsetTop = $('#skills').offset().top;
     $(window).scroll(function() {
-    var height = $(window).height();
-    if($(window).scrollTop()+height > offsetTop) {
-
-      jQuery('.bar-container span').each(function(){
-        jQuery(this).addClass('progressbar')
-      });
-
-    }else{
-      jQuery('.bar-container span').each(function(){
-        jQuery(this).removeClass('progressbar')
-      });
-    }
+        var height = $(window).height();
+        if ($(window).scrollTop() + height > offsetTop) {
+            jQuery('.bar-container span').each(function() {
+                jQuery(this).addClass('progressbar')
+            });
+        } else {
+            jQuery('.bar-container span').each(function() {
+                jQuery(this).removeClass('progressbar')
+            });
+        }
     });
-  
 
-  
- 
- 
-//portfolio
-$(".filterbtn").click(function(){
-  $(".filterbtn:not(this)").removeClass('filter-active');
-  $(this).addClass('filter-active');
- 
-  var value = $(this).attr('data-filter');
-  
-  if(value == "all")
-  {
-      $('.filter').show('1000');
-  }
-  else
-  {
-      $(".filter").not('.'+value).hide('3000');
-      $('.filter').filter('.'+value).show('3000');
-      
-  }
-});
-  
-// Testimonials carousel (uses the Owl Carousel library)
- $(".testimonials-carousel").owlCarousel({
-  autoplay: true,
-  dots: true,
-  loop: true,
-  responsive: {
-    0: {
-      items: 1
-    },
-    768: {
-      items: 2
-    },
-    900: {
-      items: 3
-    }
-  }
-});
-
-
-//navigation click for hidding navbar collaps when clicking on any navbar link
-$('.nav-link').click(function(){  
-  //  $('.navbar').removeClass('bgcolorMobilToggle', 1000);
-  // $('.navbar-collapse').collapse('hide');
-  $('.navbar').fadeOut('0', function(){   
-    $('.navbar-toggler').click();    
-    $(this).removeClass('bgcolorMobilToggle');  
-    $(this).show();
-  });  
-});
-
-
-
-// to add background for navbar 
-
-$('.navbar-toggler').click(function(){
-  if($('.navbar-toggler').hasClass("collapsed")) { 
-    $('.navbar').fadeIn('slow', function(){
-      $('.navbar').addClass('bgcolorMobilToggle');
+    /*==================Setting for section Portfolio: progress bar animated===================*/
+    $(".filterbtn").click(function() {
+        $(".filterbtn:not(this)").removeClass('filter-active');
+        $(this).addClass('filter-active');
+        var value = $(this).attr('data-filter');
+        if (value == "all") {
+            $('.filter').show('1000');
+        } else {
+            $(".filter").not('.' + value).hide('3000');
+            $('.filter').filter('.' + value).show('3000');
+        }
     });
-  }
-  else {
-    $('.navbar').fadeOut('0', function(){
-      $(this).show();
-      $(this).removeClass('bgcolorMobilToggle');      
-    });  
-  } 
-  
-})
 
 
 
-//Jquery fin
+
+
+    //Jquery fin
 });
-
-
-
