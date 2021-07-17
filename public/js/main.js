11f5a0ac7c8684d5c3b2e07f9592e03ca9fc73e3
Oumaima El Obayid <elobayidoumaima@gmail.com>
2021-07-17 21:50:51 +0100

    suite

--- public/js/main.js
+++ public/js/main.js
@@ -1,10 +1,36 @@
+// Parallax effect
+// Adapted from @ilonacodes article ->  https://link.medium.com/7fFiON6Q1X
+
+// Update : added throttle to increase performance
+window.addEventListener('scroll', throttle(parallax, 14));
+
+function throttle(fn, wait) {
+    var time = Date.now();
+    return function() {
+        if ((time + wait - Date.now()) < 0) {
+            fn();
+            time = Date.now();
+        }
+    }
+};
+
+function parallax() {
+    var scrolled = window.pageYOffset;
+    var parallax = document.querySelector(".parallax");
+    // You can adjust the 0.4 to change the speed
+    var coords = (scrolled * 0.4) + 'px'
+    parallax.style.transform = 'translateY(' + coords + ')';
+};
+
 /*============================================================================================*/
 /*=======================================jquery code==========================================*/
 /*============================================================================================*/
-$(document).ready(function() {
-    $('.venobox').venobox();
-});
+
+
+
 $(function() {
+
+
 
     /*===================================Initiation Lightbox=====================================*/
     if ($(window).width() > 992) {
