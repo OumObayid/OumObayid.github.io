766134e9bff3426db075d1677021da94d115d4b7
Oumaima El Obayid <elobayidoumaima@gmail.com>
2021-07-18 23:33:16 +0100

    suite

--- public/js/main.js
+++ public/js/main.js
@@ -21,8 +21,8 @@ function throttle(fn, wait) {
 function parallax() {
     var scrolled = window.pageYOffset;
     var parallax = document.querySelector(".parallax");
-    // You can adjust the 0.4 to change the speed
-    var coords = (scrolled * 0.7) + 'px'
+    // You can adjust the 0.8 to change the speed
+    var coords = (scrolled * 0.8) + 'px'
     parallax.style.transform = 'translateY(' + coords + ')';
 };
 
