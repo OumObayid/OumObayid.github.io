b3616b8edc07af22fb84792578a0b6bcfc39e119
Oumaima El Obayid <elobayidoumaima@gmail.com>
2021-07-20 14:04:04 +0100

    suite

--- public/php/mail.php
+++ public/php/mail.php
@@ -1,85 +1,22 @@
 <?php
-$errorMSG = "";
- 
-// NAME
-if (empty($_POST["name"])) {
-    $errorMSG = "Veuillez indiquer votre Nom et Prénom";
-} else {
-    $name = $_POST["name"];
-}
- 
-// EMAIL
-if (empty($_POST["email"])) {
-    $errorMSG .= "Veuillez indiquer votre Email";
-} else {
-    $email = $_POST["email"];
-}
+// SendPulse's PHP Library: https://github.com/sendpulse/sendpulse-rest-api-php
+require_once( 'api/sendpulseInterface.php' );
+require_once( 'api/sendpulse.php' );
+$SPApiProxy = new SendpulseApi( API_USER_ID, API_SECRET, 'file' );
 
-// TEL
-if (empty($_POST["tel"])) {
-    $errorMSG .= "Veuillez indiquer votre numéro de téléphone";
-} else {
-    $tel = $_POST["tel"];
-}
-
-// ENTREPRISE
-if (empty($_POST["entreprise"])) {
-    $errorMSG .= "Veuillez indiquer votre entreprise";
-} else {
-    $entreprise = $_POST["entreprise"];
-}
-
-
-// MESSAGE
-if (empty($_POST["message"])) {
-    $errorMSG .= "Message is required ";
-} else {
-    $message = $_POST["message"];
-}
- 
- 
-$name = $_POST["name"];
-$email = $_POST["email"];
-$tel = $_POST["tel"];
-$entreprise = $_POST["entreprise"];
-$message = $_POST["message"];
- 
-$EmailTo = "votreemail@gmail.com,autreemail@hotmail.fr";
-$Subject = "Nouveau message de monsite.com";
- 
-// le corps du mail
-$Body .= "Name: ";
-$Body .= $name;
-$Body .= "\n";
- 
-$Body .= "Email: ";
-$Body .= $email;
-$Body .= "\n";
- 
-$Body .= "Tel: ";
-$Body .= $tel;
-$Body .= "\n";
-
-$Body .= "Entreprise: ";
-$Body .= $entreprise;
-$Body .= "\n";
-
-$Body .= "Message: ";
-$Body .= $message;
-$Body .= "\n";
- 
-// envoi email
-$success = mail($EmailTo, $Subject, $Body, "From:".$email);
- 
-// redirection vers le message de validation
-if ($success && $errorMSG == ""){
-   echo "success";
-}else{
-    if($errorMSG == ""){
-        echo "Something went wrong :(";
-    } else {
-        echo $errorMSG;
-    }
-}
-
-?>
+$email = array(
+  'html'    => 'Your email content goes here',
+  'text'    => 'Your email text version goes here',
+  'subject' => 'Testing SendPulse API',
+  'from'    => array(
+    'name'  => 'Your Sender Name',
+    'email' => 'your-sender-email@gmail.com'
+  ),
+  'to'      => array(
+    array(
+      'name'  => 'Subscriber Name',
+      'email' => 'subscriber@gmail.com'
+    )
+  )
+);
+var_dump( $SPApiProxy->smtpSendMail( $email ) );
